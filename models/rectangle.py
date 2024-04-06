1cdb927142b6d63c5d02ef130cbc2fb6489c6278
Osazee25 <osazeemarvins@yahoo.com>
2024-04-06 15:18:49 -0700

    circle

--- models/rectangle.py
+++ models/rectangle.py
@@ -64,6 +64,10 @@ class Rectangle(Base):
     def display(self):
         if self.__width and self.__height == 0:
             print()
+        for k in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
